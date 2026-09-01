288652df40fdea4dfef72e9fcae2f45b066c4282
pavankishorekaranth <karanthkishore@gmail.com>
2026-09-01 05:52:59 -0500

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,7 +1,7 @@
 window._config = {
     cognito: {
-        userPoolId: 'ap-south-1_67KlJJo0k', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '1bld2lsd0c4plq7eabao4etchr', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        userPoolId: 'ap-south-1_xiKFgZ8As', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '2plsj39qdp1n2021gufq8f6en6', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
         region: 'ap-south-1' // e.g. us-east-2
     },
     api: {
